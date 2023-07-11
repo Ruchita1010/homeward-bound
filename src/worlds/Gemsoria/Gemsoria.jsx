b75aa572141199b0855fd072cdb694d4640c684e
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-07-11 15:33:10 +0530

    feat: remove the shard once it is found

--- src/worlds/Gemsoria/Gemsoria.jsx
+++ src/worlds/Gemsoria/Gemsoria.jsx
@@ -4,6 +4,7 @@ import { Cavern } from './ModelComponents/Cavern';
 import { Shard } from './ModelComponents/Shard';
 import Player from '../../components/Player';
 import { useUUID } from '../../hooks/useUUID';
+import { useHover } from '../../hooks/useHover';
 
 const Gemsoria = () => {
   const initialShardsState = [
@@ -13,8 +14,18 @@ const Gemsoria = () => {
     { position: [58, 7, -121], rotation: [0, Math.PI, 0] },
     { position: [-70, -2.4, -140], rotation: [0, 0, Math.PI] },
   ];
-  const [shards] = useState(initialShardsState);
+  const [shards, setShards] = useState(initialShardsState);
   const shardKeys = useUUID(shards.length);
+  const [handlePointerOver, handlePointerOut] = useHover('grab', 'auto');
+
+  const handleShardClick = (foundShardIndex) => {
+    const updatedShards = shards.map((shard, shardIndex) =>
+      shardIndex === foundShardIndex ? { ...shard, found: true } : shard
+    );
+    setShards(updatedShards);
+    // set the hovered state back to 'false'
+    handlePointerOut();
+  };
 
   return (
     <>
@@ -23,9 +34,19 @@ const Gemsoria = () => {
       <OrbitControls makeDefault maxDistance={12} />
       <Cavern position={[0, -10, -100]} rotation={[0, Math.PI, 0]} />
       <Player boundary={{ x1: 80, x2: 80, z1: 100, z2: 180 }} />
-      {shards.map(({ position, rotation }, index) => (
-        <Shard key={shardKeys[index]} position={position} rotation={rotation} />
-      ))}
+      {shards.map(
+        ({ position, rotation, found }, index) =>
+          !found && (
+            <Shard
+              key={shardKeys[index]}
+              position={position}
+              rotation={rotation}
+              onClick={() => handleShardClick(index)}
+              onPointerOver={handlePointerOver}
+              onPointerOut={handlePointerOut}
+            />
+          )
+      )}
     </>
   );
 };

9d32f61d9c58d8827d0cd2cdba2cc351a52f0fb7
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-07-16 00:44:24 +0530

    Updates

---
+++ assets/NewWorld-5e02a212.js
@@ -0,0 +1,1 @@
+import{j as r,H as e}from"./index-bad5f7b8.js";const t=()=>r.jsx(e,{children:r.jsx("p",{children:"New World"})});export{t as default};
--- assets/NewWorld-9118de23.js
+++
@@ -1,1 +0,0 @@
-import{j as r,H as e}from"./index-73b081cc.js";const t=()=>r.jsx(e,{children:r.jsx("p",{children:"New World"})});export{t as default};
